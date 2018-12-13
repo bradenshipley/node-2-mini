ff068cff4aa94a7988c6bf3b356fbe40165ab636
Braden Shipley <bradenrshipley@gmail.com>
2018-12-13 13:26:07 -0600

    add solutions

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+require("dotenv").config()
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const massive = require("massive")
+const controller = require("./controller")
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
 
-const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance)
+  // .new_planes()
+  // .then(planes => console.log(planes))
+  // .catch(err => console.log(err))
+//   dbInstance
+//     .get_planes()
+//     .then(planes => {
+//       console.log(planes)
+//     })
+//     .catch(err => console.log("err"))
+})
+
+app.get('/api/planes', controller.getPlanes)
 
 const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
-
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
